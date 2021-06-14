2ff1ee33b55d0a71c8e7188161ace94751db6112
abhiTronix <abhi.una12@gmail.com>
2021-06-14 08:44:06 +0530

    ✨ WebGear_RTC: Responsive Video Scaling
    - Implmented new responsive video tag scaling according to screen aspect ratios.
    - Added bootstrap css properties to implement auto-scaling.
    - Removed old `resize()` hack.
    - Beautify files syntax and updated files checksum.
    - Refractor files and removed redundandant code.
    - Bumped theme version to v0.1.2

--- README.md
+++ README.md
@@ -13,13 +13,13 @@ This repository maintains updates for additional vital binaries for [vidgear](ht
 * **Version:** v0.1.1
 * **Files MD5 Checksums:**
 	```shell
-	9e4d3304ac51b29b2f0d5dcb524a46bf  ./static/img/favicon-32x32.png
-	c4655ff907a2631bfc310e19aaeec1b8  ./static/css/custom.css
-	d45f3d577053182c162261f64ab42563  ./static/js/custom.js
-	6dd9d207b45a8fca70cced1403abbef6  ./templates/404.html
-	8334bc6d4e8a05f50c14e4f59aae8d4e  ./templates/index.html
-	0acc0384ce0422a9fee4998236676c33  ./templates/500.html
-	b5917428ca1bce7c73ffac0acf9707ce  ./templates/base.html
+	9e4d3304ac51b29b2f0d5dcb524a46bf  webgear/static/img/favicon-32x32.png
+	c4655ff907a2631bfc310e19aaeec1b8  webgear/static/css/custom.css
+	d45f3d577053182c162261f64ab42563  webgear/static/js/custom.js
+	6dd9d207b45a8fca70cced1403abbef6  webgear/templates/404.html
+	8334bc6d4e8a05f50c14e4f59aae8d4e  webgear/templates/index.html
+	0acc0384ce0422a9fee4998236676c33  webgear/templates/500.html
+	b5917428ca1bce7c73ffac0acf9707ce  webgear/templates/base.html
 	```
 
 ### WebGear_RTC default theme
@@ -29,16 +29,16 @@ This repository maintains updates for additional vital binaries for [vidgear](ht
 * **Description:** Default theme files for WebGear_RTC API.
 * **Author:** [@abhiTronix](https://github.com/abhiTronix)
 * **License:** Apache License 2.0
-* **Version:** v0.1.1
+* **Version:** v0.1.2
 * **Files MD5 Checksums:**
 	```shell
-	9e4d3304ac51b29b2f0d5dcb524a46bf  ./static/img/favicon-32x32.png
-	1aa2125fc8e50773db92ed9a42051988  ./static/css/custom.css
-	ab63d836841b4503208515b8128ea28d  ./static/js/custom.js
-	6dd9d207b45a8fca70cced1403abbef6  ./templates/404.html
-	e398f8b06ded7288cf2368a8826e3581  ./templates/index.html
-	0acc0384ce0422a9fee4998236676c33  ./templates/500.html
-	1a9e6ac8c590ceafa4a186d9ab55190e  ./templates/base.html
+	9e4d3304ac51b29b2f0d5dcb524a46bf  webgear_rtc/static/img/favicon-32x32.png
+	14a7b7cbc8b46a6f5ab26ea92955dfdf  webgear_rtc/static/css/custom.css
+	29da26abb4c0dff056139110b3163b11  webgear_rtc/static/js/custom.js
+	6dd9d207b45a8fca70cced1403abbef6  webgear_rtc/templates/404.html
+	aafa5ad625b9aa2cb8d138769ce50532  webgear_rtc/templates/index.html
+	0acc0384ce0422a9fee4998236676c33  webgear_rtc/templates/500.html
+	6645742f1f432a00f570ecc7af0a0fd2  webgear_rtc/templates/base.html
 	```
 
 &nbsp;
--- webgear_rtc/static/js/custom.js
+++ webgear_rtc/static/js/custom.js
@@ -18,108 +18,82 @@ limitations under the License.
 var pc = null;
 // define negotiate function
 function negotiate() {
-	//  creates a new RTCRtpTransceiver `video` and adds it to the 
-	//set of transceivers associated with the RTCPeerConnection
-	pc.addTransceiver('video', {
-		direction: 'recvonly'
-	});
-	// create and return  SDP offer for the purpose 
-	// of starting a new WebRTC connection to a remote peer
-	return pc.createOffer().then(function (offer) {
-		return pc.setLocalDescription(offer);
-	}).then(function () {
-		// wait for ICE gathering to complete
-		return new Promise(function (resolve) {
-			if (pc.iceGatheringState === 'complete') {
-				resolve();
-			} else {
-				function checkState() {
-					if (pc.iceGatheringState === 'complete') {
-						// remove `icegatheringstatechange` event listener
-						pc.removeEventListener('icegatheringstatechange', checkState);
-						resolve();
-					}
-				}
-				// add `icegatheringstatechange` event listener
-				pc.addEventListener('icegatheringstatechange', checkState);
-			}
-		});
-	}).then(function () {
-		// build and return json offer.
-		var offer = pc.localDescription;
-		return fetch('/offer', {
-			body: JSON.stringify({
-				sdp: offer.sdp,
-				type: offer.type,
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			method: 'POST'
-		});
-	}).then(function (response) {
-		// return response
-		return response.json();
-	}).then(function (answer) {
-		// return session description as the 
-		// remote peer's current answer. 
-		return pc.setRemoteDescription(answer);
-	}).catch(function (e) {
-		// catch any errors.
-		alert(e);
-	});
+    //  creates a new RTCRtpTransceiver `video` and adds it to the 
+    //set of transceivers associated with the RTCPeerConnection
+    pc.addTransceiver('video', {
+        direction: 'recvonly'
+    });
+    // create and return  SDP offer for the purpose 
+    // of starting a new WebRTC connection to a remote peer
+    return pc.createOffer().then(function(offer) {
+        return pc.setLocalDescription(offer);
+    }).then(function() {
+        // wait for ICE gathering to complete
+        return new Promise(function(resolve) {
+            if (pc.iceGatheringState === 'complete') {
+                resolve();
+            } else {
+                function checkState() {
+                    if (pc.iceGatheringState === 'complete') {
+                        // remove `icegatheringstatechange` event listener
+                        pc.removeEventListener('icegatheringstatechange', checkState);
+                        resolve();
+                    }
+                }
+                // add `icegatheringstatechange` event listener
+                pc.addEventListener('icegatheringstatechange', checkState);
+            }
+        });
+    }).then(function() {
+        // build and return json offer.
+        var offer = pc.localDescription;
+        return fetch('/offer', {
+            body: JSON.stringify({
+                sdp: offer.sdp,
+                type: offer.type,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        });
+    }).then(function(response) {
+        // return response
+        return response.json();
+    }).then(function(answer) {
+        // return session description as the 
+        // remote peer's current answer. 
+        return pc.setRemoteDescription(answer);
+    }).catch(function(e) {
+        // catch any errors.
+        alert(e);
+    });
 }
 // add configs
 var config = {
-	sdpSemantics: 'unified-plan'
+    sdpSemantics: 'unified-plan'
 };
 // add STUN protocol to discover your public address 
 // and determine any restrictions in your router that 
 // would prevent a direct connection with a peer.
 config.iceServers = [{
-	urls: ['stun:stun.l.google.com:19302']
+    urls: ['stun:stun.l.google.com:19302']
 }];
 // create new RTCPeerConnection, which represents 
 // a connection between the local device and a remote peer.
 pc = new RTCPeerConnection(config);
 // connect to video and assign to video element source. 
-pc.addEventListener('track', function (evt) {
-	if (evt.track.kind == 'video') {
-		document.getElementById('video').srcObject = evt.streams[0];
-	}
+pc.addEventListener('track', function(evt) {
+    if (evt.track.kind == 'video') {
+        document.getElementById('video').srcObject = evt.streams[0];
+    }
 });
 // call negotiate
 negotiate();
-// resize video tag for smaller screens[Temporary HACK]
-function resize() {
-	var video = document.getElementsByTagName('video');
-	var w = window.innerWidth / 1.5;
-	var h = window.innerHeight;
-	for (i = 0; i < video.length; i++) {
-		var ratio = (video[i].clientHeight / video[i].clientWidth);
-		if (video[i].clientHeight > h && video[i].clientWidth < w) {
-			video[i].style.height = h + "px";
-			video[i].style.width = (h / ratio) + "px";
-		}
-		if (video[i].clientHeight <= h && video[i].clientWidth < w && ratio > 1) {
-			video[i].style.height = h + "px";
-			video[i].style.width = (h / ratio) + "px";
-		}
-		if (video[i].clientWidth >= w) {
-			video[i].style.width = w + "px";
-		}
-		if (video[i].clientHeight < h && video[i].clientWidth <= w && ratio < 1) {
-			video[i].style.width = w + "px";
-		}
-	}
-}
-// call resize
-resize();
-window.onresize = resize;
 
 // Call 'close_connection' endpoint to inform server that we are refreshing page
-window.onbeforeunload = function(event)
-{
+window.onbeforeunload = function(event) {
     pc.close();
-    axios.post('/close_connection', 1)
+    pc = null;
+    axios.post('/close_connection', 1); // POST
 };
