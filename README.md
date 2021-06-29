9cce19f9a1d33137a50461cdfb54a8b76c06f001
abhiTronix <abhi.una12@gmail.com>
2021-06-29 10:17:22 +0530

    ✨ WebGear: Added Responsive Image Scaling
    - Added responsive image scaling according to screen aspect ratios.
    - Added responsive text scaling.
    - Added rounded border and auto-center to image tag.
    - Added bootstrap css properties to implement auto-scaling.
    - Removed old `resize()` hack.
    - Improved text spacing and weight.
    - Integrated toggle fullscreen to new implementation.
    - Hide Scrollbar both in WebGear_RTC and WebGear Themes.
    - Beautify files syntax and updated files checksum.
    - Refractor files and removed redundandant code.
    - Bumped theme version to v0.1.2

--- README.md
+++ README.md
@@ -10,16 +10,16 @@ This repository maintains updates for additional vital binaries for [vidgear](ht
 * **Description:** Default theme files for WebGear API.
 * **Author:** [@abhiTronix](https://github.com/abhiTronix)
 * **License:** Apache License 2.0
-* **Version:** v0.1.1
+* **Version:** v0.1.2
 * **Files MD5 Checksums:**
 	```shell
 	9e4d3304ac51b29b2f0d5dcb524a46bf  webgear/static/img/favicon-32x32.png
-	c4655ff907a2631bfc310e19aaeec1b8  webgear/static/css/custom.css
-	d45f3d577053182c162261f64ab42563  webgear/static/js/custom.js
+	46e99bb7fca3196809b80d9e111383d6  webgear/static/css/custom.css
+	97003787b3c75c99457b6dd1670f21ae  webgear/static/js/custom.js
 	6dd9d207b45a8fca70cced1403abbef6  webgear/templates/404.html
-	8334bc6d4e8a05f50c14e4f59aae8d4e  webgear/templates/index.html
+	3bff2e1c6887f92981d74f0795877195  webgear/templates/index.html
 	0acc0384ce0422a9fee4998236676c33  webgear/templates/500.html
-	b5917428ca1bce7c73ffac0acf9707ce  webgear/templates/base.html
+	e522a4a79cce593dfb8a29dba8d01635  webgear/templates/base.html
 	```
 
 ### WebGear_RTC default theme
@@ -33,12 +33,12 @@ This repository maintains updates for additional vital binaries for [vidgear](ht
 * **Files MD5 Checksums:**
 	```shell
 	9e4d3304ac51b29b2f0d5dcb524a46bf  webgear_rtc/static/img/favicon-32x32.png
-	14a7b7cbc8b46a6f5ab26ea92955dfdf  webgear_rtc/static/css/custom.css
+	a0fac71498524647558447cfdfc6c953  webgear_rtc/static/css/custom.css
 	29da26abb4c0dff056139110b3163b11  webgear_rtc/static/js/custom.js
 	6dd9d207b45a8fca70cced1403abbef6  webgear_rtc/templates/404.html
 	aafa5ad625b9aa2cb8d138769ce50532  webgear_rtc/templates/index.html
 	0acc0384ce0422a9fee4998236676c33  webgear_rtc/templates/500.html
-	6645742f1f432a00f570ecc7af0a0fd2  webgear_rtc/templates/base.html
+	2b8434ccdde8cc3e063a2d2509d02ee3  webgear_rtc/templates/base.html
 	```
 
 &nbsp;
--- webgear/static/js/custom.js
+++ webgear/static/js/custom.js
@@ -2,46 +2,20 @@
 ===============================================
 vidgear library source-code is deployed under the Apache 2.0 License:
 Copyright (c) 2019 Abhishek Thakur(@abhiTronix) <abhi.una12@gmail.com>
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-===============================================
-*/
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+    ===============================================
+    */
 
+/*handles fullscreen workflow*/
 function toggleFullScreen(id) {
     const element = document.getElementById(id);
     screenfull.toggle(element)
 }
-
-// resize img tag for smaller screens[Temporary HACK]
-function resize() {
-	var img = document.getElementsByTagName('img');
-	var w = window.innerWidth / 1.5;
-	var h = window.innerHeight;
-	for (i = 0; i < img.length; i++) {
-		var ratio = (img[i].clientHeight / img[i].clientWidth);
-		if (img[i].clientHeight > h && img[i].clientWidth < w) {
-			img[i].style.height = h + "px";
-			img[i].style.width = (h / ratio) + "px";
-		}
-		if (img[i].clientHeight <= h && img[i].clientWidth < w && ratio > 1) {
-			img[i].style.height = h + "px";
-			img[i].style.width = (h / ratio) + "px";
-		}
-		if (img[i].clientWidth >= w) {
-			img[i].style.width = w + "px";
-		}
-		if (img[i].clientHeight < h && img[i].clientWidth <= w && ratio < 1) {
-			img[i].style.width = w + "px";
-		}
-	}
-}
-// call resize
-resize();
-window.onresize = resize;
